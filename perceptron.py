d806be917c27ffcdded2607e791f1ccfa19f4cc0
tgielbutowicz <tomasz.gielbutowicz@gmail.com>
2015-03-18 20:08:26 +0100

    Init

---
+++ perceptron.py
@@ -0,0 +1,10 @@
+__author__ = 'Brutus'
+
+class Perceptron:
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)

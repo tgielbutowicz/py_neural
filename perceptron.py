1a1f4ae7d44efb61d9d57e01edc5b72db8038433
tgielbutowicz <tomasz.gielbutowicz@gmail.com>
2015-03-18 20:43:29 +0100

    sigmoidelFunction

--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,10 @@
 __author__ = 'Brutus'
+import math
 
 class Perceptron:
-    def __init__(self, weights):
-        self.name = weights
+    def __init__(self, weights, activationTreshold):
+        self.weights = weights
+        self.activationTreshold = activationTreshold
+
+    def sigmoidelFunction(x, alfa = 1):
+        1.0 / (1.0 + math.pow(math.e,-x*alfa))
